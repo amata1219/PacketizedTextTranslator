a30bdab9053ff24da84f603416d055e9d3f61d18
amata1219 <suzukiamata@gmail.com>
2020-09-20 10:52:52 +0900

    こみっとし祭

--- src/amata1219/packetized/text/translator/PlayerListener.scala
+++ src/amata1219/packetized/text/translator/PlayerListener.scala
@@ -1,18 +1,15 @@
 package amata1219.packetized.text.translator
 
+import amata1219.packetized.text.translator.packet.TranslatorInjection
 import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.event.player.{PlayerJoinEvent, PlayerQuitEvent}
 
 object PlayerListener extends Listener {
 
   @EventHandler
-  def on(event: PlayerJoinEvent): Unit = {
-
-  }
+  def on(event: PlayerJoinEvent): Unit = TranslatorInjection.applyTo(event.getPlayer)
 
   @EventHandler
-  def on(event: PlayerQuitEvent): Unit = {
-
-  }
+  def on(event: PlayerQuitEvent): Unit = TranslatorInjection.unapplyTo(event.getPlayer)
 
 }
---
+++ src/amata1219/packetized/text/translator/packet/TranslatorInjection.scala
@@ -0,0 +1,31 @@
+package amata1219.packetized.text.translator.packet
+
+import amata1219.xeflection.Reflect
+import io.netty.channel.ChannelPipeline
+import org.bukkit.entity.Player
+
+object TranslatorInjection {
+
+  val identifier: String = "PacketizedTextTranslator"
+
+  def applyTo(player: Player): Unit = {
+    val pipeline: ChannelPipeline = player.pipeline
+    if (pipeline.get(identifier) == null) pipeline.addBefore("packet_handler", identifier, new SendingPacketHandler(player))
+  }
+
+  def unapplyTo(player: Player): Unit = {
+    val pipeline: ChannelPipeline = player.pipeline
+    if (pipeline.get(identifier) != null) pipeline.remove(identifier)
+  }
+
+  implicit class XPlayer(player: Player) {
+    def pipeline: ChannelPipeline = Reflect.on(player)
+      .call("getHandle")
+      .field("playerConnection")
+      .field("networkManager")
+      .field("channel")
+      .call("pipeline")
+      .as[ChannelPipeline]
+  }
+
+}
